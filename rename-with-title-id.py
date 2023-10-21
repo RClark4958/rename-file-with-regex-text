21c6dfa234aa71330a3f6536df9295a7287d5711
Rick Clark <rickdclark48@gmail.com>
2023-10-20 19:56:48 -0600

    feat: ignores invalid characters

--- rename-with-title-id.py
+++ rename-with-title-id.py
@@ -1,46 +1,55 @@
 import os
 import re
 
-# The directory where your subdirectories are located.
-# You can replace this with your desired directory.
-main_dir_path = r"D:"
+def get_string_from_nfo(folder_path):
+    """
+    Retrieve the desired string from the .nfo file in the given folder.
 
-# Pattern for the title ID in the .nfo file
-pattern = re.compile(r'(0100[^\s]*)')
+    Args:
+        folder_path (str): Path to the folder to be checked for an .nfo file.
 
-# Iterate over each subdirectory in the main directory
-for dir_name in os.listdir(main_dir_path):
-    dir_path = os.path.join(main_dir_path, dir_name)
-    if os.path.isdir(dir_path):
-        # This variable will hold the Title ID once it's found
-        title_id = None
+    Returns:
+        str or None: Desired string that starts with '0100' or None if not found.
+    """
+    # Loop through all files in the folder
+    for filename in os.listdir(folder_path):
+        # If the current file is an .nfo file, process it
+        if filename.endswith('.nfo'):
+            # Open the .nfo file, ignoring any unrecognized characters during decoding
+            with open(os.path.join(folder_path, filename), 'r', errors='ignore') as file:
+                content = file.read()
+                # Use regex to find a string starting with '0100' and not followed by whitespaces
+                match = re.search(r'0100[^\s]*', content)
+                if match:
+                    return match.group(0)  # Return the found string
+    return None  # Return None if no matching string is found
 
-        # Iterate over every file in the subdirectory
-        for filename in os.listdir(dir_path):
-            # If the file is a .nfo file
-            if filename.endswith('.nfo'):
-                with open(os.path.join(dir_path, filename), 'r') as f:
-                    # Read the contents of the file
-                    contents = f.read()
-                    # Search for the Title ID
-                    match = pattern.search(contents)
-                    # If found, store it in the title_id variable
-                    if match:
-                        title_id = match.group(1)
-                        break
+def main():
+    # Prompt the user to input the directory's path
+    base_directory = input("Enter the path of the directory: ")
 
-        # If the Title ID was found
-        if title_id:
-            # Iterate over every file in the directory again
-            for filename in os.listdir(dir_path):
-                # If the file is a .xci or .nsp file
-                if filename.endswith('.xci') or filename.endswith('.nsp'):
-                    # Split the filename into name and extension
-                    name, ext = os.path.splitext(filename)
-                    # Create a new filename using the Title ID and the extension
-                    new_filename = f'{title_id}{ext}'
-                    # Check if the new file name already exists
-                    if not os.path.exists(os.path.join(dir_path, new_filename)):
-                        # Rename the file
-                        os.rename(os.path.join(dir_path, filename),
-                                  os.path.join(dir_path, new_filename))
+    # Loop through all items (files/folders) in the given directory
+    for folder_name in os.listdir(base_directory):
+        folder_path = os.path.join(base_directory, folder_name)
+        
+        # Check if the current item is a directory/folder
+        if os.path.isdir(folder_path):
+            # Retrieve the desired string from the .nfo file in this folder
+            desired_string = get_string_from_nfo(folder_path)
+            
+            # If a desired string was found
+            if desired_string:
+                # List all .xci or .nsp files in the current folder
+                files_to_rename = [file for file in os.listdir(folder_path) if file.endswith('.xci') or file.endswith('.nsp')]
+                
+                # Rename each of these files with the desired string
+                for file in files_to_rename:
+                    new_filename = desired_string + os.path.splitext(file)[1]
+                    os.rename(os.path.join(folder_path, file), os.path.join(folder_path, new_filename))
+                    # Assuming only one .xci or .nsp file needs to be renamed per directory
+                    break
+
+if __name__ == "__main__":
+    # Execute the main function when the script is run
+    main()
+
